ec6521e2109e8969d0f1ae5865a913b033263067
DaniilVenikov <daniilvenikoff@gmail.com>
2025-03-05 09:28:06 +0300

    Brought to a corrected decision

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,63 +1,115 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
-abstract class General<T extends General<T>> {
-
-    public abstract T add(T other);
-}
-
-class Vector<T extends General<T>> {
-    private final T[] values;
-
-    public Vector(T[] values) {
-        this.values = values;
-    }
-
-    public Vector<T> add(Vector<T> other, Class clazz) {
-        if (this.values.length != other.values.length) {
-            return null;
-        }
-
-        T[] result = (T[]) Array.newInstance(clazz, values.length);
-
-        for (int i = 0; i < values.length; i++) {
-            result[i] = (values[i].add(other.values[i]));
-        }
-        return new Vector<>(result);
-    }
-
-    @Override
-    public String toString() {
-        return "Vector{" +
-                "values=" + Arrays.toString(values) +
-                '}';
-    }
-}
-
-
-class IntValue extends General<IntValue> {
-    private final int value;
-
-    public IntValue(int value) {
-        this.value = value;
-    }
-
-    @Override
-    public IntValue add(IntValue other) {
-        return new IntValue(this.value + other.value);
-    }
-
-    @Override
-    public String toString() {
-        return String.valueOf(value);
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        Vector<IntValue> v1 = new Vector<>(new IntValue[]{new IntValue(1), new IntValue(2), new IntValue(3)});
-        Vector<IntValue> v2 = new Vector<>(new IntValue[]{new IntValue(4), new IntValue(5), new IntValue(6)});
-        System.out.println(v1.add(v2, IntValue.class));
 
+    }
+}
+
+class Adder<T> extends Any {
+
+    public T sum(T first, T second){
+
+        T ans = null;
+
+        if(first instanceof String){
+            ans = sumString(first, second);
+        }
+
+        if(first instanceof Integer){
+            ans = sumInteger(first, second);
+        }
+
+        if (first instanceof Double){
+            ans = sumDouble(first, second);
+        }
+
+        return ans;
+    }
+
+    private T sumString(T first, T second){
+        return (T)(first + (String)second);
+    }
+
+    private T sumInteger(T first, T second){
+        Integer sum = (Integer)first + (Integer)second;
+        T value = (T)sum;
+        return value;
+    }
+
+    private T sumDouble(T first, T second){
+        return (T)((Double)((Double)(first) + (Double)second));
+    }
+}
+
+class Vector<T> extends Adder {
+
+    public static int ADD_NIL = 0;
+    public static int ADD_OK = 1;
+    public static int ADD_ERR = 2;
+
+    private int length;
+    private T[] arr;
+    private int add_status;
+
+    public Vector(T[] arr){
+        this.arr = arr;
+        length = arr.length;
+        add_status = ADD_NIL;
+    }
+
+    public Vector(int length){
+        arr = (T[])new Object[length];
+        this.length = length;
+        add_status = ADD_NIL;
+    }
+
+    public void add(Vector<? extends T> v){
+        Vector<String> temp = new Vector<>(1);
+        if (v.getLength() == length){
+            T[] arr2 = v.getArr();
+
+            for (int i = 0; i < length; i++){
+                if(arr2[i].getClass().isInstance(temp)){
+                    // проверяем типы. Если это Vector, то:
+                    ((Vector<T>)arr[i]).add(((Vector<T>)arr2[i]));
+                }
+                else { // иначе - это Number или String
+                    add_v((Vector<T>) v);
+                    add_status = ADD_OK;
+                    break;
+                }
+            }
+
+            add_status = ADD_OK;
+        }
+        else{
+            add_status = ADD_ERR;
+        }
+    }
+
+    private void add_v(Vector<T> v){
+        T[] arr2 = v.getArr();
+        for(int i = 0; i < length; i++){
+            arr[i] = (T) sum(arr[i], arr2[i]);
+        }
+    }
+
+    public static Vector addVectors(Vector v1, Vector v2) throws Exception {
+        Vector ans = v1.deepClone();
+
+        ans.add(v2);
+
+        return ans;
+    }
+
+    public int getLength(){
+        return length;
+    }
+
+    public int get_add_status(){
+        return add_status;
+    }
+
+    public T[] getArr(){
+        return arr;
     }
 }
